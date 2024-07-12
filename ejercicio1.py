46b4bcc85d8251e486486eb0595eeabca8d13b6c
Luis Dorado <luisedorado1976@gmail.com>
2024-07-12 14:48:02 -0500

    ejercicio para participacion foro

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+pregunta1 = input ("Escribe tu color favorito en femenimo :")
+pregunta2 = input ("Escribe tu ciudad favarita : ")
+print (("Esta tomando la mejor cervrza del pais y se llama : \"") + pregunta1 +" "+"de"+" " +pregunta2+"\"")
